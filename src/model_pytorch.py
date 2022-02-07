5ce44e8026c6dc54ca08cd10ecda1005ea8f027f
charlyalizadeh <charlypro1502@outlook.fr>
2022-02-07 23:36:08 +0100

    Add classes to experiment with pytorch models

---
+++ src/model_pytorch.py
@@ -0,0 +1,167 @@
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from OPFDataset import OPFDataset
+import random
+import warnings
+import copy
+from pytorch_util import train, predict
+from collections import defaultdict
+
+
+class ModelExperimentFFNN:
+    def __init__(self, model, is_classifier):
+        self.model = model
+        self.is_classifier = is_classifier
+        self.cuda = next(model.parameters()).is_cuda
+
+    def setup_data(self, opf=False, opf_category=None,
+                   nb_instance_test=1, random_state_test=42,
+                   treshold=-0.4, batch_size=256,
+                   nb_instance_val=1, random_state_val=42):
+        dataset = OPFDataset()
+        if not opf:
+            dataset.remove_OPF_features()
+        if opf_category is not None:
+            dataset = dataset.split_per_pattern(opf_category)
+        if self.is_classifier:
+            dataset.categorize(treshold, set_target=True)
+
+        # Split train/test/val
+        if nb_instance_test > 0:
+            random.seed(random_state_test)
+            test_instances = random.choices(dataset['instance_name'].unique(), k=nb_instance_test)
+            self.test = dataset.split_per_instance(test_instances)
+        else:
+            self.test = OPFDataset()
+            self.test.empty()
+        if nb_instance_val > 0:
+            random.seed(random_state_val)
+            val_instances = random.choices(dataset['instance_name'].unique(), k=nb_instance_val)
+            self.val = dataset.split_per_instance(val_instances)
+        else:
+            self.val = OPFDataset()
+            self.val.empty()
+
+        # Remove non features columns
+        dataset.remove_non_features()
+        self.test.remove_non_features()
+        self.val.remove_non_features()
+
+        # Scale
+        scaler = MinMaxScaler()
+        dataset.fit_scaler(scaler)
+        self.test.scale(scaler)
+        self.val.scale(scaler)
+
+        self.train = dataset
+
+        # Check for any imbalanced target
+        if self.is_classifier:
+            for i in range(2):
+                if len(self.train[self.train["target"] == i].index) == 0:
+                    warnings.warn(f"The train set has no category {i} target")
+                if len(self.test.df.index) != 0 and len(self.test[self.test["target"] == i].index) == 0:
+                    warnings.warn(f"The test set has no category {i} target")
+                if len(self.val.df.index) != 0 and len(self.val[self.val["target"] == i].index) == 0:
+                    warnings.warn(f"The val set has no category {i} target")
+
+        self.train_loader = self.train.get_torch_loader(batch_size=batch_size, shuffle=False, cuda=self.cuda)
+        self.test_loader = self.test.get_torch_loader(batch_size=batch_size, shuffle=False, cuda=self.cuda)
+        self.val_loader = self.val.get_torch_loader(batch_size=batch_size, shuffle=False, cuda=self.cuda)
+
+    def fit(self, epochs, optimizer, criterion, lr):
+        train_losses, val_losses = train(self.model, self.train_loader, self.val_loader, epochs, optimizer, criterion, lr)
+        return train_losses, val_losses
+
+    def evaluate(self, dataset, metrics={}):
+        metrics_values = {}
+        loader = self.train_loader if dataset == 'train' else self.test_loader
+        y_true, y_pred = predict(self.model, loader)
+        y_true = y_true.cpu().detach().numpy()
+        y_pred = y_pred.cpu().detach().numpy()
+        y_pred = np.where(y_pred < 0.5, 0, 1)
+        for m in metrics:
+            if m.__name__ == 'mergeSort':
+                sort_perm = np.argsort(y_pred)
+                metrics_values[m.__name__] = m(y_true[sort_perm].tolist()) / y_true.shape[0]
+            else:
+                metrics_values[m.__name__] = m(y_true, y_pred)
+        return metrics_values
+
+
+class ModelExperimentFFNNCV:
+    def __init__(self, model, is_classifier):
+        self.model = model
+        self.is_classifier = is_classifier
+        self.cuda = next(model.parameters()).is_cuda
+
+    def setup_data(self, nb_instance_per_split=3, shuffle=False, opf=False, opf_category=None,
+                   random_state=42, treshold=-0.4, batch_size=256):
+        dataset = OPFDataset()
+        if not opf:
+            dataset.remove_OPF_features()
+        if opf_category is not None:
+            dataset = dataset.split_per_pattern(opf_category)
+        if self.is_classifier:
+            dataset.categorize(treshold, set_target=True)
+
+        # Split train/test
+        self.folds = dataset.fold_per_instance(nb_instance_per_split, shuffle,
+                                               random_state, opf=opf, remove_non_features=True)
+
+        # Scale
+        for train_fold, test_fold in self.folds:
+            scaler = MinMaxScaler()
+            train_fold.fit_scaler(scaler)
+            test_fold.scale(scaler)
+
+        # Check for any imbalanced target
+        if self.is_classifier:
+            for fold_i, (train_fold, test_fold) in enumerate(self.folds):
+                for i in range(2):
+                    if len(train_fold[train_fold["target"] == i].index) == 0:
+                        warnings.warn(f"The train set of the fold {fold_i} has no category {i} target")
+                    if len(test_fold[test_fold["target"] == i].index) == 0:
+                        warnings.warn(f"The test set of the fold {fold_i} has no category {i} target")
+
+        self.folds_loader = [
+                (train.get_torch_loader(cuda=self.cuda, batch_size=batch_size, shuffle=shuffle),
+                 test.get_torch_loader(cuda=self.cuda, batch_size=batch_size, shuffle=shuffle))
+                for train, test in self.folds
+        ]
+
+    def cross_val(self, metrics={}, epochs=500, optimizer='RMSprop', criterion='binary', lr=1e-3, **kwargs):
+        train_metrics = defaultdict(list)
+        test_metrics = defaultdict(list)
+        for i, (train_loader, test_loader) in enumerate(self.folds_loader):
+            print(f'FOLD {i}')
+            train_metrics['fold_id'].append(i)
+            test_metrics['fold_id'].append(i)
+
+            # Model initialization
+            model = copy.deepcopy(self.model)
+
+            # Train
+            train_losses, test_losses = train(model, train_loader, test_loader,
+                                              epochs, optimizer, criterion, lr)
+            train_metrics['losses'].append(train_losses.tolist())
+            test_metrics['losses'].append(test_losses.tolist())
+            y_train, y_train_pred = predict(model, train_loader, to_numpy=True)
+            y_test, y_test_pred = predict(model, test_loader)
+
+            # Evaluate
+            train_metrics['y_true'].append(y_train)
+            train_metrics['y_pred'].append(y_train_pred)
+            test_metrics['y_true'].append(y_test)
+            test_metrics['y_pred'].append(y_test_pred)
+            for metric_func, metric_kwargs in metrics.items():
+                metric_name = metric_func.__name__
+                if metric_name == 'mergeSort':
+                    train_sort_perm = np.argsort(y_train_pred)
+                    test_sort_perm = np.argsort(y_test_pred)
+                    train_metrics[metric_name].append(metric_func(y_train[train_sort_perm].tolist()) / y_train.shape[0])
+                    test_metrics[metric_name].append(metric_func(y_test[test_sort_perm].tolist()) / y_test.shape[0])
+                else:
+                    train_metrics[metric_name].append(metric_func(y_train, y_train_pred))
+                    test_metrics[metric_name].append(metric_func(y_test, y_test_pred))
+        return train_metrics, test_metrics
---
+++ src/pytorch_util.py
@@ -0,0 +1,66 @@
+import torch
+from torch import tensor
+import torch.nn as nn
+from torch.optim import Adam, RMSprop
+
+optimizers = {
+    'Adam': Adam,
+    'RMSprop': RMSprop
+}
+losses = {
+    'crossentropy': nn.CrossEntropyLoss,
+    'binary': nn.BCELoss,
+}
+
+
+def train(model, train_loader, val_loader, epochs, optimizer, criterion, lr):
+    optimizer = optimizers[optimizer](model.parameters(), lr=lr)
+    criterion = losses[criterion]()
+    train_losses = tensor([]).cpu()
+    val_losses = tensor([]).cpu()
+
+    for epoch in range(epochs):
+        train_step(model, train_loader, criterion, optimizer)
+        with torch.no_grad():
+            train_loss = score(model, train_loader, criterion)
+            val_loss = score(model, val_loader, criterion)
+            train_losses = torch.cat((train_losses, torch.tensor([train_loss.cpu()])), 0)
+            val_losses = torch.cat((val_losses, torch.tensor([val_loss.cpu()])), 0)
+            print(f"Epoch: {epoch:03d}, Train Loss: {train_loss:.4f} Val Loss: {val_loss:.4f}")
+    return train_losses, val_losses
+
+
+def train_step(model, loader, criterion, optimizer):
+    model.train()
+    for i, (X, y) in enumerate(loader):
+        print(f"    {i + 1} / {len(loader)}", end="\r")
+        X, y = X.to('cuda'), y.to('cuda')
+        out = torch.flatten(model(X))
+        loss = criterion(out, y)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+    print()
+
+
+def predict(model, loader, to_numpy=False):
+    with torch.no_grad():
+        model.eval()
+        y_true = tensor([]).cuda()
+        y_pred = tensor([]).cuda()
+        for X, y in loader:
+            X, y = X.to('cuda'), y.to('cuda')
+            out = torch.flatten(model(X))
+            y_pred = torch.cat((y_pred, out), 0)
+            y_true = torch.cat((y_true, y), 0)
+    if to_numpy:
+        y_true = y_true.detach().cpu().numpy()
+        y_pred = y_pred.detach().cpu().numpy()
+    return y_true, y_pred
+
+
+def score(model, loader, criterion):
+    with torch.no_grad():
+        model.eval()
+        y_true, y_pred = predict(model, loader)
+        return criterion(y_pred, y_true)
